84afff10463709f0c8cf0dd64b92f2ab3424a60b
Rafael Madeira <sirminsc@gmail.com>
2021-05-09 15:29:41 +0200

    Fixed Navigation css instead of style embeded into nav tag

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Navigation.css'
 
 const Navigation = () => {
     return (
          /*
          * Building sign out button with a bit css and tachyons (like bootstrap)
          */
-        <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+        <nav>
             <p className='f3 link dim black underline pa3 pointer'>Sign Out</p>
         </nav>
 
